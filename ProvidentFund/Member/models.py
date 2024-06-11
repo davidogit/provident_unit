c33a912c0de0fc9379a343f1e70e0cad0573d20c
Freemane-Mawuli <tettehfreeman21@gmail.com>
2024-06-11 08:06:16 -0700

    update

--- ProvidentFund/Member/models.py
+++ ProvidentFund/Member/models.py
@@ -7,7 +7,6 @@ class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     staff_id = models.PositiveIntegerField(unique=True, null=False, blank=False)
     tel_number = models.PositiveIntegerField()
-    name = models.CharField(max_length=40)
 
     def __str__(self):
         return f'{self.user.username}\'s account'
