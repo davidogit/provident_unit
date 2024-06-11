fafa87a1849392243198f27fb5acf2c9fbe7ff42
Freemane-Mawuli <tettehfreeman21@gmail.com>
2024-06-11 08:04:10 -0700

    free1

--- ProvidentFund/Member/models.py
+++ ProvidentFund/Member/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    staff_id = models.PositiveIntegerField(unique=True, null=False, blank=False, max_length=10)
-    tel_number = models.PositiveIntegerField(max_length=10)
+    staff_id = models.PositiveIntegerField(unique=True, null=False, blank=False)
+    tel_number = models.PositiveIntegerField()
+    name = models.CharField(max_length=40)
 
     def __str__(self):
         return f'{self.user.username}\'s account'
