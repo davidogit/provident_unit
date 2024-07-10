5cd9de4bb39de2d299afee045f74ead074c1f098
davidogit <65226873+davidogit@users.noreply.github.com>
2024-07-10 11:42:47 +0000

    update

--- ProvidentFund/contributions/models.py
+++ ProvidentFund/contributions/models.py
@@ -3,8 +3,6 @@ from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
-from django.db import models
-
 class StaffAPI(models.Model):
     Id = models.BigIntegerField(primary_key=True, unique=True)
     Fullname = models.CharField(max_length=255)
@@ -12,10 +10,10 @@ class StaffAPI(models.Model):
     Datejoined = models.DateTimeField(auto_now=True)
     status = models.IntegerField()
     Fundtype = models.CharField(max_length=50)
-    EmployeeAmount = models.DecimalField(max_digits=10, decimal_places=2)
-    EmployerAmount = models.DecimalField(max_digits=10, decimal_places=2)
-    RetroEmployeeAmount = models.DecimalField(max_digits=10, decimal_places=2)
-    RetroEmployerAmount = models.DecimalField(max_digits=10, decimal_places=2)
+    EmployeeAmount = models.FloatField()
+    EmployerAmount = models.FloatField()
+    RetroEmployeeAmount = models.FloatField()
+    RetroEmployerAmount = models.FloatField()
     ContributionDate = models.DateTimeField(auto_now=True)
     ExitedDate = models.DateTimeField(null=True, blank=True)
     ExitedFlag = models.BooleanField(default=False)
@@ -27,16 +25,27 @@ class StaffAPI(models.Model):
     def contributions(self):
         return self.contribution_set.all()
 
+    @property
+    def total_contributions(self):
+        total = self.contributions.aggregate(
+            total=models.Sum(
+                models.F('EmployeeAmount') + 
+                models.F('EmployerAmount') + 
+                models.F('RetroEmployeeAmount') + 
+                models.F('RetroEmployerAmount')
+            )
+        )['total'] or 0
+        return total
+
 class Contribution(models.Model):
     member = models.ForeignKey(StaffAPI, on_delete=models.CASCADE, related_name='contributions')
     month = models.CharField(max_length=20)
     year = models.CharField(max_length=4)
-    EmployeeAmount = models.DecimalField(max_digits=10, decimal_places=2)
-    EmployerAmount = models.DecimalField(max_digits=10, decimal_places=2)
-    RetroEmployeeAmount = models.DecimalField(max_digits=10, decimal_places=2)
-    RetroEmployerAmount = models.DecimalField(max_digits=10, decimal_places=2)
+    EmployeeAmount = models.FloatField()
+    EmployerAmount = models.FloatField()
+    RetroEmployeeAmount = models.FloatField()
+    RetroEmployerAmount = models.FloatField()
     ContributionDate = models.DateTimeField()
-
 
     def calculated_total_contributions(self):
         a = self.EmployeeAmount
@@ -54,6 +63,3 @@ class Contribution(models.Model):
 
     def __str__(self):
         return f'{self.member.Fullname} - {self.month} {self.year}'
-
-
-
