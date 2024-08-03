cba8b818520d5e7ecac63ba36af5bf3d33947b24
Huvison <huvisoncollins@gmail.com>
2024-08-03 18:53:24 +0000

    middleware update to check integers

--- ProvidentFund/Fund/middleware.py
+++ ProvidentFund/Fund/middleware.py
@@ -8,39 +8,67 @@ class URLTenantMiddleware(MiddlewareMixin):
     def process_request(self, request):
         path_parts = request.path.split('/')
         # Ensure admin is not affected by tenant_id
-        if 'favicon.ico' not in path_parts:
-            if 'admin' not in path_parts:
-                if 'admin_panel' not in path_parts:
-
-                    if len(path_parts)>1:
-                        # Get Tenant from url
-                        tenant_id = int(path_parts[1])
-
-                        # Get Scheme name from url
-                        if len(path_parts)>3:
-                            if not path_parts[3] =='':
-                                scheme_name = int(path_parts[3])
-                                request.scheme_name = scheme_name
-                            else:
-                                request.scheme_name = None
-                                
-                        else:
-                            request.scheme_name = None
-
-                        try:
-                            request.tenant = Tenant.objects.get(id=tenant_id)
-                        except Tenant.DoesNotExist:
-                            request.tenant = None 
-                    else: 
-                        request.scheme_name = None 
-                else:
+        if len(path_parts)>1:
+            if isinstance(int(path_parts[1]), int):
+                # Get Tenant from url
+                tenant_id = int(path_parts[1])
+                try:
+                    request.tenant = Tenant.objects.get(id=tenant_id)
+                except Tenant.DoesNotExist:
                     request.tenant = None
-                    request.scheme_name = None
             else:
-                request.tenant = None
-                request.scheme_name = None
+                 request.tenant = None
+        else: 
+            request.scheme_name = None 
+
+        # Get Scheme name from url
+        if len(path_parts)>3:
+            if isinstance(int(path_parts[3]),int):
+                if not path_parts[3] =='':
+                    scheme_name = int(path_parts[3])
+                    request.scheme_name = scheme_name
+                else:
+                    request.scheme_name = None 
+            else:
+                 request.scheme_name = None
         else:
-            request.tenant = None
             request.scheme_name = None
+        
 
 
+
+
+# if 'favicon.ico' not in path_parts:
+#             if 'admin' not in path_parts:
+#                 if 'admin_panel' not in path_parts:
+
+#                     if len(path_parts)>1:
+#                         # Get Tenant from url
+#                         tenant_id = int(path_parts[1])
+
+#                         # Get Scheme name from url
+#                         if len(path_parts)>3:
+#                             if not path_parts[3] =='':
+#                                 scheme_name = int(path_parts[3])
+#                                 request.scheme_name = scheme_name
+#                             else:
+#                                 request.scheme_name = None
+                                
+#                         else:
+#                             request.scheme_name = None
+
+#                         try:
+#                             request.tenant = Tenant.objects.get(id=tenant_id)
+#                         except Tenant.DoesNotExist:
+#                             request.tenant = None 
+#                     else: 
+#                         request.scheme_name = None 
+#                 else:
+#                     request.tenant = None
+#                     request.scheme_name = None
+#             else:
+#                 request.tenant = None
+#                 request.scheme_name = None
+#         else:
+#             request.tenant = None
+#             request.scheme_name = None
