59346ccd8ce3906dce4ad055629050c4c52009be
Huvison <huvisoncollins@gmail.com>
2025-06-08 19:16:46 +0000

    chore: (loan) add amortization table for loans, add logic to disburse loan

--- ProvidentFund/Loan/urls.py
+++ ProvidentFund/Loan/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('apply/<int:staff_id>/', views.HandleLoanSubmission.as_view()),
     path('approve-loans/',views.LoanApprovalView.as_view(), name='approve_loans'),
     path('disburse_approved_loans/', views.DisburseApprovedLoans.as_view(), name='disburse_approved_loans'),
-    path('approved-loan-details/<int:approved_loan_id>/',views.FetchLoanDetails.as_view()),
+    path('loan-details/<int:loan_id>/',views.FetchLoanDetails.as_view()),
     path('rejected-applications/', views.RejectedLoanApplications.as_view(), name='rejected_loans'),
     path('loan-types/', views.LoanTypeView.as_view(), name='loan_types'),
     path('add-loan-type/', views.CreateLoanType.as_view(),name='add_loan_type'),
@@ -18,4 +18,5 @@ urlpatterns = [
     path('<int:staff_id>/loan_types/', views.MemberLoanTypeView.as_view(), name='member_loan_type'),
     path('fetch_loan_type_details/', views.FetchLoanTypeDetails.as_view(), name='fetch_loan_types'),
     path('<int:staff_id>/member-loan-details/<int:loan_id>/', views.MemberLoanDetailView.as_view(), name='member_loan_details'),
+    path('disbursed-loans/', views.DisbursedLoans.as_view(), name='disbursed_loans'),
 ]
